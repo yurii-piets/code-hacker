53ca8f20042cfbf54eac58774f58f184d2574d8b
Yurii Piets <yurii.piets@gmail.com>
2017-05-14 22:52:10 +0100

    Fixed bug with LEDs.

--- hacker/hacker.ino
+++ hacker/hacker.ino
@@ -5,11 +5,12 @@ const int strobe = 7;
 const int clock = 9;
 const int data = 8;
 
-TM1638 module(data, clock, strobe);
 const int DISPLAY_SIZE = 9;
+char code[DISPLAY_SIZE] = "AbCdE321";
+TM1638 module(data, clock, strobe);
 const char allowedChars[] = {'0','1','2','3','4','5','6','7','8','9','A','b','C','d','E','F'}; 
 
-
+//Three posible states of program
 const int WAITING = 0;
 const int IN_PROGRESS = 1;
 const int FINISHED = 2;
@@ -21,30 +22,16 @@ void setup() {
   module.clearDisplay();
 }
 
-char* getRandomArray(){
-  char current [DISPLAY_SIZE];
-
-  for(int i=0; i < DISPLAY_SIZE; ++i){
-    int randomValue = random(0, 16);
-    current[i] = allowedChars[randomValue];
-  }
-  
-  return current;
-}
-
-//A b C d E F
 void loop() {
   char display[DISPLAY_SIZE];
-  char code[DISPLAY_SIZE] = "b10A16F7";
   int leds = 0;
-  
+  int MIN_OCCURANCES = 10;
+
   for(int i = 0; i < DISPLAY_SIZE; ++i){
     char *randomArray = "--------";
-    
-    leds |= (int) pow(2, i - 1);    
-    module.setLEDs(leds);
-    
-    while(randomArray[i] != code[i]){
+
+    int count = 0;
+    while(count++ < MIN_OCCURANCES || randomArray[i] != code[i] ){
       randomArray = getRandomArray();
       
       for(int j = 0; j < DISPLAY_SIZE; ++j){
@@ -53,9 +40,12 @@ void loop() {
       module.setDisplayToString(display);
       currentState = IN_PROGRESS;
       listenKeys();
-      delay(100);
+      module.setLEDs(leds);
       listenKeys();
+      delay(100);
     }
+    listenKeys();
+    leds |= pow2(i);
   }
   currentState = FINISHED;
   listenKeys();
@@ -63,7 +53,7 @@ void loop() {
 
 void listenKeys(){
   do{
-    int keys = module.getButtons();
+    int keys = module.getButtons(); 
     if(keys == 1){
       switch(currentState){
         case IN_PROGRESS:
@@ -94,4 +84,25 @@ void listenKeys(){
         break;
     }
   }while(1);
+}
+
+char* getRandomArray(){
+  char current [DISPLAY_SIZE];
+
+  for(int i=0; i < DISPLAY_SIZE; ++i){
+    int randomValue = random(0, 16);
+    current[i] = allowedChars[randomValue];
+  }
+  
+  return current;
+}
+
+int pow2(int p){ 
+  int result = 1;
+  
+  for(int i = 1; i <= p ; ++i){
+    result *=2;
+  }
+
+  return result;
 }
