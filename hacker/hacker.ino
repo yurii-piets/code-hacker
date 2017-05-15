34459a66aee48e7b0e8f377f0fe1e189b1de0a2a
Yurii Piets <yurii.piets@gmail.com>
2017-05-15 01:56:57 +0100

    #Removed S1 and S2 buttons handlers. #Was fixed bug with display sizing

--- hacker/hacker.ino
+++ hacker/hacker.ino
@@ -1,28 +1,69 @@
 #include <TM1638.h>
 
+
+char CODE[] = "3E6F5678";
+int DISP = 200;
+
 // Ustawienia pinów
 const int strobe = 7;
 const int clock = 9;
 const int data = 8;
 
-TM1638 module(data, clock, strobe);
 const int DISPLAY_SIZE = 9;
-const char allowedChars[] = {'0','1','2','3','4','5','6','7','8','9','A','b','C','d','E','F'}; 
+const int STEP_DISP = 10;
+const char allowedChars[] = {'0','1','2','3','4','5','6','7','8','9','A','b','C','d','E','F'};
 
+TM1638 module(data, clock, strobe);
 
-const int WAITING = 0;
-const int IN_PROGRESS = 1;
-const int FINISHED = 2;
+//Definicje funkcji
+char* getRandomArray();
+int pow2(int);
+void step_delay();
 
-int currentState = IN_PROGRESS;
 
 void setup() {
   Serial.begin(9600);
   module.clearDisplay();
 }
 
+void loop() {
+  char *disp = (char *) malloc(DISPLAY_SIZE);
+  int leds = 0;
+  Serial.println("hello");
+  module.clearDisplay();
+  module.setLEDs(leds);
+  
+  for(int i = 0; i < DISPLAY_SIZE-1; ++i){    
+    char *randomArray = getRandomArray();
+    while(randomArray[i] != CODE[i]){
+      free(randomArray);
+      randomArray = getRandomArray();
+
+      for(int j = 0; j < DISPLAY_SIZE-1; ++j){
+         disp[j] = j < i ? CODE[j] : randomArray[j];
+      }
+      
+      module.setDisplayToString(disp);
+      step_delay();
+    }
+
+    leds |= (int) pow2(i);    
+    module.setLEDs(leds);
+    free(randomArray);
+  }
+  
+  free(disp);
+  Serial.println("bye");
+}
+
+void step_delay(){
+  for(int k = 0; k < DISP; k += STEP_DISP){        
+    delay(STEP_DISP);
+  }
+}
+
 char* getRandomArray(){
-  char current [DISPLAY_SIZE];
+  char *current = (char *) malloc(DISPLAY_SIZE);
 
   for(int i=0; i < DISPLAY_SIZE; ++i){
     int randomValue = random(0, 16);
@@ -34,77 +75,14 @@ char* getRandomArray(){
 
 int pow2(int p){ 
   if(p < 0){
-    return;
+    return 0;
   }
+  
   int result = 1;
   
-  for(int i = 1; i <= p ; ++i){
+  for(int i = 0; i < p ; ++i){
     result *=2;
   }
 
   return result;
-}
- 
-
-void loop() {
-  char display[DISPLAY_SIZE];
-  char code[DISPLAY_SIZE] = "b10A16F7";
-  int leds = 0;
-  
-  for(int i = 0; i < DISPLAY_SIZE; ++i){
-    char *randomArray = "--------";
-    
-    module.setLEDs(leds);
-    
-    while(randomArray[i] != code[i]){
-      randomArray = getRandomArray();
-      
-      for(int j = 0; j < DISPLAY_SIZE; ++j){
-        display[j] = j < i ? code[j] : randomArray[j];
-      }
-      module.setDisplayToString(display);
-      currentState = IN_PROGRESS;
-      listenKeys();
-      delay(100);
-      listenKeys();
-    }
-    leds |= (int) pow2(i);    
-  }
-  currentState = FINISHED;
-  listenKeys();
-}
-
-void listenKeys(){
-  do{
-    int keys = module.getButtons();
-    if(keys == 1){
-      switch(currentState){
-        case IN_PROGRESS:
-          currentState = WAITING;
-          break;
-        case WAITING:
-          currentState = IN_PROGRESS;
-          break;
-        default:
-          break;
-      }
-    }else if (keys == 2){
-      if(currentState == FINISHED){
-        currentState = IN_PROGRESS;
-      }
-    }
-  
-    switch(currentState){
-      case IN_PROGRESS:
-        return;
-      case WAITING:
-        delay(50);
-        break;
-      case FINISHED:
-        delay(50);
-        break;
-      default:
-        break;
-    }
-  }while(1);
 }
